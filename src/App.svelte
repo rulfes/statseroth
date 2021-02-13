c059accd0a08901edd1e53ee301b4e80a375fddc
Rolf Ammann <rodaam@protonmail.ch>
2021-02-13 11:23:36 +0100

    following the tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,105 @@
 <script>
-	export let name;
+  import { onMount} from "svelte";
+  let username;
+  let password;
+  let isAuthenticated = false;
+  let csrf = document.getElementsByName("csrf-token")[0].content;
+  onMount(() => {
+    fetch("http://localhost:5000/api/getsession", {
+      credentials: "same-origin",
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.login == true) {
+        isAuthenticated = true;
+      } else {
+        isAuthenticated = false;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  });
+  const login = () => {
+    fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrf,
+      },
+      credentials: "same-origin",
+      body: JSON.stringify({ username: username, password: password }),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.login == true) {
+        isAuthenticated = true;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+  const whoami = () => {
+    fetch("/api/data", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrf,
+      },
+      credentials: "same-origin",
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      alert(`Welcome, ${data.username}!`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+  const logout = () => {
+    fetch("/api/logout", {
+      credentials: "same-origin",
+    })
+    .then(() => {
+      isAuthenticated = false;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
 </script>
+
+<style>
+  .container {
+    padding-top: 10%;
+  }
+</style>
+
+<center>
+  <div class="container">
+    {#if isAuthenticated}
+      <h1>You are authenticated!</h1>
+      <button type="button" on:click={whoami}>whoami</button>
+      <button type="button" on:click={logout}>logout</button>
+    {:else}
+      <h1>Log in</h1>
+      <form id="form">
+        username:
+        <input type="text" bind:value={username} />
+        <br /><br />
+        password:
+        <input type="password" bind:value={password} /><br /><br />
+        <button type="button" on:click={login}>login</button>
+      </form>
+    {/if}
+  </div>
+</center>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
