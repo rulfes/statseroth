fe1cd5ae459cd4889614411b0b377e5275acbe85
Rolf Ammann <rodaam@protonmail.ch>
2021-02-13 11:26:51 +0100

    making the tutorial things actually work

--- src/App.svelte
+++ src/App.svelte
@@ -98,8 +98,3 @@
     {/if}
   </div>
 </center>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
